39cb10860bf45743f69791e7dc76ef3eb13f037c
zc_glb <zcglbgood@zc.com>
2018-10-31 13:37:14 +0800

    方式

--- src/main/java/zcn/zc/git/People.java
+++ src/main/java/zcn/zc/git/People.java
@@ -4,6 +4,6 @@ public class People {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("2222");
-			System.out.println("1211");
+		System.out.println("1211");
 	}
 }
